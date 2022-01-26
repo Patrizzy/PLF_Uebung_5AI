e3acab475b0c891734017a38e2be80a5c7b81983
F. Kasper <ksp@htl.rennweg.at>
2022-01-26 11:28:59 +0100

    Client: einzelne Entity über REST-API laden

--- client/src/services/rest.js
+++ client/src/services/rest.js
@@ -1,11 +1,20 @@
 import axios from 'axios'
 import Page from '@/models/Page'
-import SongModel from '@/models/Song'
 
 // Basis-URL aller REST-API-Endpunkte
 const API_BASE = 'http://localhost:8080/api'
 
 
+/**
+ * Liefert ein Promise auf eine Seite von Entities des
+ * angegebenen Typs. Existiert die Seite nicht, so wird
+ * die letzte vorhandene Seite geliefert.
+ *
+ * Argumente (optional, wenn nicht anders angegeben):
+ *   Entity        (erforderlich) Entity-Klasse, liefert auch den Pfad im REST-API
+ *   pageNum       Nummer der zu ladenden Seite (erste Seite: 0)
+ *   params        Namen und Werte der Request-Parameter als Objekt
+ */
 export function loadPage(Entity, pageNum = 0, params = {}) {
     return axios
         .get(
@@ -30,6 +39,37 @@ export function loadPage(Entity, pageNum = 0, params = {}) {
 }
 
 
+/**
+ * Lädt über das REST-API vom Server eine einzelne Entity des
+ * angegebenen Typs und liefert ein Promise auf das geladene Objekt.
+ *
+ * Argumente (optional, wenn nicht anders angegeben):
+ *   Entity        (erforderlich) Entity-Klasse
+ *   href          (erforderlich) REST-API-Link zur Entity
+ *   parameter     Namen und Werte der Request-Parameter als Objekt
+ */
+export function loadEntity(Entity, href, params) {
+    return axios
+        .get(href, { params })
+        .then(response => {
+            const result = new Entity(response.data)
+            console.log('rest.loadEntity() OK', result)
+            return result
+        })
+        .catch(response => {
+            console.error('rest.loadEntity() error', response)
+            return Promise.reject(response)
+        })
+}
+
+
+/**
+ * Aktualisiert oder erzeugt die angegebene Entity auf dem Server,
+ * je nachdem, ob sie bereits vom Server stammt oder lokal erzeugt
+ * wurde.
+ *
+ * Liefert ein Promise auf die aktuelle Version der Entity.
+ */
 export function saveEntity(entity) {
     const promise = entity.isNew()
         ? axios.post(`${API_BASE}/${entity.constructor.path}`, entity)
@@ -50,6 +90,11 @@ export function saveEntity(entity) {
 }
 
 
+/**
+ * Löscht die angegebene Entity von Server.
+ *
+ * Liefert ein Promise auf den Erfolg.
+ */
 export function deleteEntity(entity) {
     return axios
         .delete(entity._links.self.href)

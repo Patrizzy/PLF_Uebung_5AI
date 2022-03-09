7935cd9d906d078f6ae1557fe3c647f1c0064929
Franz Stimpfl <stf@htl.rennweg.at>
2022-03-09 11:21:20 +0100

    Anpassung des Song-Models zur korrekten Verwendung im Editor-State

--- client/src/models/Song.js
+++ client/src/models/Song.js
@@ -1,9 +1,16 @@
 import Persistent from '@/models/Persistent'
+import Artist from "@/models/Artist"
 
 export default class Song extends Persistent {
 
     constructor(obj) {
         super(obj)
+
+        if(this.isNew()) {
+            this.artist = new Artist()
+            this.genres = []
+        }
+
     }
 
 }
